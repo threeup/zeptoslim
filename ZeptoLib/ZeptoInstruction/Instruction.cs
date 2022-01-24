3810415e19687094e446ea91a278f77e24087a7e
Sheridan <threeup@gmail.com>
2022-01-23 16:22:06 -0800

    comments and print

--- ZeptoLib/ZeptoInstruction/Instruction.cs
+++ ZeptoLib/ZeptoInstruction/Instruction.cs
@@ -1,54 +1,69 @@
-
-
-
 namespace ZeptoInstruction;
 
 public class Instruction
 {
-  public int depth = 0;
-  public Condition condition;
-  public string verb;
+    public int depth = 0;
+    public Condition condition;
+    public string verb;
 
-  private List<Instruction>? subInstructions = null;
-  private ZeptoFormula.Formula? formula = null;
+    private List<Instruction>? subInstructions = null;
+    private string comments = "";
+    private ZeptoFormula.Formula? formula = null;
 
-  public Instruction(int depth)
-  {
-    this.condition = Condition.NONE;
-    this.verb = "";
-    this.depth = depth;
-  }
-
-  public Instruction(int depth, Condition condition)
-  {
-    this.condition = condition;
-    this.verb = "";
-    this.depth = depth;
-  }
-  public Instruction(int depth, string verb)
-  {
-    this.condition = Condition.NONE;
-    this.verb = verb;
-    this.depth = depth;
-  }
-
-  public void AddSubInstruction(Instruction sub)
-  {
-    if (subInstructions == null)
+    public Instruction(int depth)
     {
-      subInstructions = new List<Instruction>();
+        this.condition = Condition.NONE;
+        this.verb = "";
+        this.depth = depth;
     }
-    subInstructions.Add(sub);
-  }
 
-  public void AddFormula(ZeptoFormula.Formula formula)
-  {
-    this.formula = formula;
-  }
+    public Instruction(int depth, Condition condition)
+    {
+        this.condition = condition;
+        this.verb = "";
+        this.depth = depth;
+    }
+    public Instruction(int depth, string verb)
+    {
+        this.condition = Condition.NONE;
+        this.verb = verb;
+        this.depth = depth;
+    }
 
-  public string ToLongString()
-  {
-    int subCount = subInstructions != null ? subInstructions.Count : 0;
-    return "Inst Depth:" + depth + " sub:" + subCount;
-  }
+    public void AddSubInstruction(Instruction sub)
+    {
+        if (subInstructions == null)
+        {
+            subInstructions = new List<Instruction>();
+        }
+        subInstructions.Add(sub);
+    }
+
+    public void AddFormula(ZeptoFormula.Formula formula)
+    {
+        this.formula = formula;
+    }
+    public void AddComments(string comments)
+    {
+        this.comments = comments;
+    }
+
+    public string ToLongString(int shift = 0)
+    {
+        System.Text.StringBuilder sb = new System.Text.StringBuilder();
+        for (int i = 0; i < shift; ++i)
+        {
+            sb.Append(' ');
+        }
+        int subCount = subInstructions != null ? subInstructions.Count : 0;
+        sb.AppendLine("Inst " + verb + "." + this.condition + " Depth:" + depth + " sub:" + subCount + " //" + comments);
+        if (subInstructions != null)
+        {
+            foreach (Instruction instr in subInstructions)
+            {
+                sb.AppendLine(instr.ToLongString(shift + 3));
+            }
+        }
+        return sb.ToString();
+    }
 }
--- ZeptoLib/ZeptoInstruction/UnitTest.cs
+++ ZeptoLib/ZeptoInstruction/UnitTest.cs
@@ -1,4 +1,5 @@
 
+
 using ZeptoCommon;
 using ZeptoFormula;
 
@@ -6,62 +7,70 @@ namespace ZeptoInstruction;
 
 public static class UnitTest
 {
-  public static void Run()
-  {
-    TestZero();
-    TestOne();
-    TestTwo();
-    TestThree();
-    Console.WriteLine("Instruction Test Passed");
-  }
+    public static void Run()
+    {
+        TestZero();
+        TestOne();
+        TestTwo();
+        TestThree();
+        Console.WriteLine("Instruction Test Passed");
+    }
 
-  public static void TestZero()
-  {
-    string varContents = "HP, , ";
-    string verbContents = " ";
-    string[] prepContents = new string[] { "" };
-    string[] bodyContents = new string[] { "4" };
+    public static void TestZero()
+    {
+        string varContents = "HP, , ";
+        string verbContents = " ";
+        string[] prepContents = new string[] { "" };
+        string[] bodyContents = new string[] { "4" };
 
-    IInstructionContext ctx = InstructionFactory.MakeContext(varContents, verbContents, prepContents);
-    List<Instruction> instrList = new List<Instruction>();
-    List<string> buffer = new List<string>();
-    InstructionFactory.MakeList(ctx, bodyContents, ref instrList, ref buffer);
+        IInstructionContext ctx = InstructionFactory.MakeContext(varContents, verbContents, prepContents);
+        List<Instruction> instrList = new List<Instruction>();
+        List<string> buffer = new List<string>();
+        InstructionFactory.MakeList(ctx, bodyContents, ref instrList, ref buffer);
 
-  }
-  public static void TestOne()
-  {
-    string varContents = "HP,ENERGY";
-    string verbContents = "explode";
-    string[] prepContents = new string[] { "HP=1", "ENERGY=1" };
-    string[] bodyContents = new string[] { "if HP > 1", "  HP = 3", "ENERGY = 3" };
+    }
+    public static void TestOne()
+    {
+        string varContents = "HP,ENERGY";
+        string verbContents = "explode";
+        string[] prepContents = new string[] { "HP=1", "ENERGY=1" };
+        string[] bodyContents = new string[] { "if HP > 1", "  HP = 3", "ENERGY = 3" };
 
-    IInstructionContext ctx = InstructionFactory.MakeContext(varContents, verbContents, prepContents);
+        IInstructionContext ctx = InstructionFactory.MakeContext(varContents, verbContents, prepContents);
 
-    List<Instruction> instrList = new List<Instruction>();
-    List<string> buffer = new List<string>();
-    InstructionFactory.MakeList(ctx, bodyContents, ref instrList, ref buffer);
+        List<Instruction> instrList = new List<Instruction>();
+        List<string> buffer = new List<string>();
+        InstructionFactory.MakeList(ctx, bodyContents, ref instrList, ref buffer);
+        foreach (Instruction inst in instrList)
+        {
+            Console.WriteLine(inst.ToLongString());
+        }
 
-    //if HP > 1:
-    //    HP = 3
-    //    ENERGY = 1
+        //if HP > 1:
+        //    HP = 3
+        //    ENERGY = 1
 
-    //if explode(3) > laser(HP):
-    //    HP = explode(1) + laser(2)
-  }
-  public static void TestTwo()
-  {
-    string varContents = "HP,ENERGY";
-    string verbContents = "explode,laser";
-    string[] prepContents = new string[] { "HP=1", "ENERGY=1" };
-    string[] bodyContents = new string[] { "if explode(3) > laser(HP)", "  HP = explode(1) + laser(2)", "ENERGY = 3" };
+        //if explode(3) > laser(HP):
+        //    HP = explode(1) + laser(2)
+    }
+    public static void TestTwo()
+    {
+        string varContents = "HP,ENERGY";
+        string verbContents = "explode,laser";
+        string[] prepContents = new string[] { "HP=1", "ENERGY=1" };
+        string[] bodyContents = new string[] { "if explode(3) > laser(HP)", "  HP = explode(1) + laser(2)", "ENERGY = 3" };
 
 
-    IInstructionContext ctx = InstructionFactory.MakeContext(varContents, verbContents, prepContents);
-    List<Instruction> instrList = new List<Instruction>();
-    List<string> buffer = new List<string>();
-    InstructionFactory.MakeList(ctx, bodyContents, ref instrList, ref buffer);
-  }
-  public static void TestThree()
-  {
-  }
+        IInstructionContext ctx = InstructionFactory.MakeContext(varContents, verbContents, prepContents);
+        List<Instruction> instrList = new List<Instruction>();
+        List<string> buffer = new List<string>();
+        InstructionFactory.MakeList(ctx, bodyContents, ref instrList, ref buffer);
+        foreach (Instruction inst in instrList)
+        {
+            Console.WriteLine(inst.ToLongString());
+        }
+    }
+    public static void TestThree()
+    {
+    }
 }
